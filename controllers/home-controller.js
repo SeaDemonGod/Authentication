9ebd7d7091a257c67da507a41d4875fcde31416f
SeaDemonGod <107745612+SeaDemonGod@users.noreply.github.com>
2022-10-11 16:06:03 +0530

    added controller for home

---
+++ controllers/home-controller.js
@@ -0,0 +1,11 @@
+// home controller 
+module.exports.home = function(request, response){
+
+    // if user already signed in redirect to profile page 
+    if(request.isAuthenticated()){
+       return response.redirect('/users/profile');
+   }
+
+   return response.render('home'); 
+
+} 
